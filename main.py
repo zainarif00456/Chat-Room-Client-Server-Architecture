4eb6c7ca2fb997bf2bbc6aa1d4d068fd5d043529
Zain_Ul_Abdeen <zainarif00456@outlook.com>
2021-09-17 00:17:17 +0500

    Project Initialization

---
+++ main.py
@@ -0,0 +1,10 @@
+#       In this program, we will write code for our project.
+"""
+        Project Title:  Chat Room Using Client-Server Architecture
+        Team Members:
+            Zain-Ul-Abdeen (BCSM-F19-154)
+            M. Umair Anjum (BCSM-F19-030)
+            Fida Hussain (BCSM-F19-128)
+        Course: Computer Networks
+        Project Type:   Network Programming
+"""
