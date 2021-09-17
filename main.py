a53c9100bf8314abf17707c11cdc6c3e59cad38f
Zain-Ul-Abdeen456 <58590698+Zain-Ul-Abdeen456@users.noreply.github.com>
2021-09-17 21:31:07 +0500

    Update main.py
    Slight Change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #       In this program, we will write code for our project.
 """
         Project Title:  Chat Room Using Client-Server Architecture
-        Team Members:
+        Group Members:
             Zain-Ul-Abdeen (BCSM-F19-154)
             M. Umair Anjum (BCSM-F19-030)
             Fida Hussain (BCSM-F19-128)
