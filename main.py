16158a855b966b66e27331559ff7bf6c8a56200d
Zain_Ul_Abdeen <zainarif00456@outlook.com>
2021-09-17 22:28:02 +0500

    UserInterface Commit 01

--- main.py
+++ main.py
@@ -8,3 +8,32 @@
         Course: Computer Networks
         Project Type:   Network Programming
 """
+from tkinter import *       #       Library for User Interface
+class SignUp_Page(Tk):      #       Class of Sign up page.
+    """
+    This class contains the user interface of signup page of our chat room application.
+    """
+
+
+    def __init__(self):
+        self.width = 650
+        self.height = 400
+        super().__init__()
+        self.initialize_components()
+    def initialize_components(self):
+        """
+        This function initialize the user interface of Sign up page.
+        :return:
+        """
+        self.geometry("650x400")
+        self.maxsize(self.width, self.height)
+        self.minsize(self.width, self.height)
+        self.title("Sign Up-Chat Room")
+        
+
+
+if __name__ == '__main__':
+    sp = SignUp_Page()
+    sp.mainloop()
+
+
